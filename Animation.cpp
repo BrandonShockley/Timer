d305440e158e1a4ac9d45478280d62ea9c8d2152
Brandon Shockley (PC) <bshoc@bellsouth.net>
2015-12-07 22:49:29 -0500

    Version 1.0.3
    Polished up collision. Added basic animation.

---
+++ Animation.cpp
@@ -0,0 +1,29 @@
+#include "Animation.h"
+#include "Error.h"
+
+Animation::Animation(const std::string path, const int frames)
+{
+	sf::Image img;
+	if (!img.loadFromFile(path))
+		fatalError("Error loading image: " + path);
+	sf::Vector2u size(img.getSize().x / frames, img.getSize().y);
+	for (int i = 0; i < frames; i++)
+	{
+		sf::Texture tex;
+		tex.loadFromImage(img, sf::IntRect(i*size.x, 0, size.x, size.y));
+		frames_.push_back(tex);
+	}
+}
+
+Animation::~Animation()
+{
+}
+
+sf::Texture& Animation::getNextFrame()
+{
+	if (currentFrame < frames_.size())
+		currentFrame++;
+	else
+		currentFrame = 1;
+	return frames_[currentFrame - 1];
+}
---
+++ Animation.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include <vector>
+#include <string>
+class Animation
+{
+public:
+	Animation(const std::string path, const int frames);
+	~Animation();
+	sf::Texture& getNextFrame();
+private:
+	std::vector<sf::Texture> frames_;
+	unsigned int currentFrame = 1;
+};
+
